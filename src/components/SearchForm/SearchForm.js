8e5bf4ed5b98e62f276be1fe47d1d341320d511c
Lukasz Reglinski <lukasz.reglinski@gmail.com>
2024-04-25 15:55:01 +0200

    Add card finder function

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,23 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+
+  const dispatch = useDispatch();
+
+  const [searchString, setSearchString] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: searchString });
+  }
+
   return (
-      <form className={styles.SearchForm}>
-          <TextInput placeholder='Search…' />
+      <form className={styles.SearchForm} onSubmit={handleSubmit}>
+          <TextInput value={searchString} onChange={e => setSearchString(e.target.value)} placeholder='Search…' />
           <Button>
             <span className="fa fa-search" />
           </Button>
