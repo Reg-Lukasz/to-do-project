1010ba9ec197517dac7921689dc3eeac8d567f68
Lukasz Reglinski <lukasz.reglinski@gmail.com>
2024-04-27 14:54:40 +0200

    Add utils to projects and change code in components to use redux selectors and action creators

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -3,6 +3,7 @@ import styles from './CardForm.module.scss';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import { useDispatch } from 'react-redux';
+import { addCard } from '../../redux/store';
 
 const CardForm = props => {
 
@@ -14,7 +15,7 @@ const CardForm = props => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch({ type: 'ADD_CARD', payload: { columnId, title }});
+    dispatch(addCard({ columnId, title }));
     setTitle('');
   };
 
---
+++ src/utils/strContains.js
@@ -0,0 +1,5 @@
+export const strContains = ( title, searchString ) => {
+  if(title.toLowerCase().includes(searchString.toLowerCase())){
+    return true;
+  }
+}
